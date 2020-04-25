ade043d51a307b9376f03f41a92bda8219f9432b
GitToh <toh.mimax3@gmail.com>
2020-04-25 21:03:13 +0700

    update project

--- first.py
+++ first.py
@@ -1,17 +1,21 @@
-# comment ไทย
-"""
-Comment Ctrl+/
-"""
+# แสดงภาษาไทย
+print("สวัสดีไพทอน")
 print("Hello Python")
-print("Hello Python")
+
+
+
+a = 1
 
 print(2+3)
 print(2**3)
 print('WELCOME', end=" ")
 print('PYTHON')
 
-print('Python '*3)
+print('Python '*5)
 print(sum(range(1, 10)))
 print('\t\tIT GENIUS')
 print('This is '+'Saturday')
 print('Coffee', 'Tea', 'Milk')
+
+# import sys
+# print(sys.stdout.encoding)
